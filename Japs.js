93adce90a1956bf62006723df7b3f23a56a0d228
Mogomotsi X <87011258+mogomotsigumede@users.noreply.github.com>
2022-06-06 15:11:27 +0200

    Narrative

---
+++ Japs.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<body>
+
+<style> 
+html{
+    font-family: Inverted Sqaure; 
+    font-size: 25px;
+    background-color: Green;
+    }
+    </style>
+<h1> Japanese Paintings </h1> 
+<p3> the other thing is that Japanese art doesn't even take care of the stuff that can bring justice in the work place so far 
+it can make. I have never been to japan but it hands so much inoformation that I cant wait to go see. I have <sup> Not Studied </sup> Which 
+is just the reason to make sure that I do not have to comment so much on it that it can just make this at lot worst. 
+</p3>
+<center>
+<br>
+<img src = "http://1.bp.blogspot.com/-RvfrRVrVT3o/T6HnJDI9uDI/AAAAAAAAS2g/OlFvFZscZPk/s1600/utagawa%2Bkunisada.jpg" />
+ <br><br>  <p2> Please comment on the paint if you wait too. </p2>s<input type="Range"> <br><Br>
+  <img src = "https://lh4.ggpht.com/Hlb2rZH0MlFh6iL-o3bxQA0QJW0S5wb90v_9WVMvuQE2bvL0M71KS5_QhbnA"/>
+  <br><br> <p2> Please comment on the paint if you want too. </p2> <input type="Range">
+  <img src = "https://images.squarespace-cdn.com/content/v1/53b9abe9e4b0366641161844/1539959733929-FQNDG6TDKKUPQ9AZLTIF/Chikanobu+War++DETAIL.jpg"/>
+<br>  <p2> Please comment on the paint if you wait too. </p2> <input type="Range">
+</center>
+<center>
+<h1> This is an introduction </h3><hr>
+<br>
+    <p3> Japan has been subject to sudden invasions of new ideas followed by long periods of minimal contact with the outside world. Over time the Japanese 
+developed the ability to absorb, imitate, and finally assimilate those elements of foreign culture that complemented their aesthetic preferences. 
+The earliest complex art in Japan was produced in the 7th and 8th centuries in connection with Buddhism. In the 9th century, as the Japanese began 
+to turn away from China and develop indigenous forms of expression, the secular arts became increasingly important; until the late 15th century, both 
+religious and secular arts flourished. After the Ōnin War (1467–1477), Japan entered a period of political, social, and economic disruption that lasted 
+for over a century. In the state that emerged under the leadership of the Tokugawa shogunate, organized religion played a much less important role
+in people's lives, and the arts that survived were primarily secular. The Meiji Period (1868-1912) saw an abrupt influx of Western styles, 
+which have continued to be important. </p3>
+<br></center>
+<br><br>
+  <cite> Copyright 2019 </cite><br>
+  <cite> Property of Bill Gate and WiKipedia </cite>
+</body>
+</html>
